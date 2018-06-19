7ee1517a0865660eec949af8919b90996285eba4
Yuki <k701user@gmail.com>
2018-06-19 11:21:27 +0900

    Update talk_style.py

--- talk_style.py
+++ talk_style.py
@@ -14,6 +14,15 @@ from janome.tokenizer import Tokenizer
 talk_apikey = "E1HZR7u2fuhdtBbrATJN5yBYK5atFTl9"
 client = pya3rt.TalkClient(talk_apikey)
 
+sample_members = {
+                  "ななし":["かな", "性別", "年齢", "職業","特技","Rating","portforio url"],
+                  "揚出 剛史":["あげいでたけし", "male", "47", "医師", "対話による体調診断をおこないます。", "15", "https://www.google.com/"],
+                  "川内谷 茜音":["かわうちやあかね", "female", "51", "主婦", "料理をしにいきます。", "3", "https://www.yahoo.co.jp/"],
+                  "谷合 沙樹":["たにあいさき", "unknown", "private", "PM", "PG講座を行います。", "5", "https://www.msn.com/ja-jp"],
+                  "旭爪 昌典":["ひのつめまさのり", "male", "60", "リタイア", "おはなし相手になります。", "1", "https://www.bing.com/"],
+                  "乙幡 涼香":["おとはたすずか", "female", "41", "シェフ", "料理教室の講師を請け負います。", "33", "https://www.yahoo.co.jp/"]
+                 }
+
 class talker:
     def __init__(self, parent=None):
         pass
@@ -31,10 +40,120 @@ class talker:
                      "source": "reply"}
         
         return json_dict
+   
+    @staticmethod
+    def listup_member(push):
+        try:
+            print(push)
+            keys = morphological_analysis(push)
+            
+            # member_list = pickup_members(keys)
+            '''
+            現在サンプルなのでリストはランダムに三人出しておく
+            '''
+            member_list = []
+            for i in range(3):
+                name, profile = random.choice(list(sample_members.items()))
+                member_list.append([name, profile])    
+            
+            post_carousel(member_list)
     
+    '''
+    形態素解析
+    textを入れると、形容詞と名詞のリストを出てきた回数が多い順に返す。
+    '''
+    @staticmethod
+    def morphological_analysis(text):
+        txt = text
+        t = Tokenizer()
+        word_dic = {}
+        lines = txt.split("\r\n")
+        for line in lines:
+            blog_txt = t.tokenize(line)
+            for w in blog_txt:
+                word = w.base_form
+                ps = w.part_of_speech
+                if ps.find('名詞') < 0 and ps.find('形容詞') < 0:
+                    continue
+                if word not in word_dic:
+                    word_dic[word] = 0
+                word_dic[word] += 1
 
+        keys = sorted(word_dic.items(), key=lambda x: x[1], reverse=True)
+        keyword = []
+        for word, cnt in keys:
+            print("{0} ".format(word))
+            keyword.append(word)
+
+        return keyword
+    
+    @staticmethod
+    def post_carousel(member_list):
+        header = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {ENTER_ACCESS_TOKEN}"
+        }
+        payload = {
+              "replyToken":"",
+              "messages":[
+                  {
+                    "type": "template",
+                    "altText": "members",
+                    "template": {
+                        "type": "carousel",
+                        "columns": [
+
+                            {
+                              "thumbnailImageUrl": "http://pictogram2.com/p/p0{0:03d}/1.jpg".format(random.random(100, 800)),
+                              "title": member_list[0][0] + "(" + member_list[0][1][0] + ")",
+                              "text":  member_list[0][1][3] + ":" + member_list[0][1][4] + "(" + member_list[0][1][5] + ")",
+                              "actions": [
+
+                                  {
+                                      "type": "uri",
+                                      "label": "詳細を見る",
+                                      "uri": member_list[0][1][-1]
+                                  }
+                              ]
+                            },
+                            {
+                              "thumbnailImageUrl": "http://pictogram2.com/p/p0{0:03d}/1.jpg".format(random.random(100, 800)),
+                              "title": member_list[1][0] + "(" + member_list[1][1][0] + ")",
+                              "text":  member_list[1][1][3] + ":" + member_list[1][1][4] + "(" + member_list[1][1][5] + ")",
+                              "actions": [
+
+                                  {
+                                      "type": "uri",
+                                      "label": "詳細を見る",
+                                      "uri": member_list[1][1][-1]
+                                  }
+                              ]
+                            },
+                            {
+                              "thumbnailImageUrl": "http://pictogram2.com/p/p0{0:03d}/1.jpg".format(random.random(100, 800)),
+                              "title": member_list[2][0] + "(" + member_list[2][1][0] + ")",
+                              "text":  member_list[2][1][3] + ":" + member_list[2][1][4] + "(" + member_list[2][1][5] + ")",
+                              "actions": [
+
+                                  {
+                                      "type": "uri",
+                                      "label": "詳細を見る",
+                                      "uri": member_list[2][1][-1]
+                                  }
+                              ]
+                            }
+
+                        ]
+                    }
+                  }
+                ]
+        }
+        req = requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
+    
+    
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
