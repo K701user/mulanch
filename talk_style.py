40ab3a5435e67676e6692f767a86a0fa4004302d
Yuki <k701user@gmail.com>
2018-06-25 20:14:30 +0900

    Update talk_style.py

--- talk_style.py
+++ talk_style.py
@@ -148,15 +148,6 @@ class talker:
                         title= member_list[0][0] + "(" + member_list[0][1][0] + ")",
                         text= member_list[0][1][3] + ":" + member_list[0][1][4] + "(" + member_list[0][1][5] + ")",
                         actions=[
-                            PostbackAction(
-                                label='postback1',
-                                text='postback text1',
-                                data='action=buy&itemid=1'
-                            ),
-                            MessageAction(
-                                label='message1',
-                                text='message text1'
-                            ),
                             URIAction(
                                 label='member_portfolio',
                                 uri=member_list[0][1][-1]
@@ -168,15 +159,6 @@ class talker:
                         title= member_list[1][0] + "(" + member_list[1][1][0] + ")",
                         text= member_list[1][1][3] + ":" + member_list[1][1][4] + "(" + member_list[1][1][5] + ")",
                         actions=[
-                            PostbackAction(
-                                label='postback2',
-                                text='postback text2',
-                                data='action=buy&itemid=2'
-                            ),
-                            MessageAction(
-                                label='message2',
-                                text='message text2'
-                            ),
                             URIAction(
                                 label='member_portfolio',
                                 uri=member_list[1][1][-1]
@@ -188,15 +170,6 @@ class talker:
                         title= member_list[2][0] + "(" + member_list[2][1][0] + ")",
                         text= member_list[2][1][3] + ":" + member_list[2][1][4] + "(" + member_list[2][1][5] + ")",
                         actions=[
-                            PostbackAction(
-                                label='postback3',
-                                text='postback text3',
-                                data='action=buy&itemid=3'
-                            ),
-                            MessageAction(
-                                label='message3',
-                                text='message text3'
-                            ),
                             URIAction(
                                 label='member_portfolio',
                                 uri=member_list[2][1][-1]
